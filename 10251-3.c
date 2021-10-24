4f1308ec7d00b434d8d062881fb419da950a6670
Pangfeng Liu <pangfeng.liu@gmail.com>
2021-10-24 10:02:43 +0800

    RPG

---
+++ 10251-3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+ 
+#define DPS 0
+#define HEALER 1
+#define TANK 2
+#define CLASS 3
+ 
+#define QUEUE 5000
+ 
+bool enoughPlayer(int length[], int numP[])
+{
+  for (int c = 0; c < CLASS; c++)
+    if (length[c] < numP[c])
+      return false;
+  return true;
+}
+ 
+int main()
+{
+  int start[CLASS] = {0};
+  int length[CLASS] = {0};
+  int queue[CLASS][QUEUE];
+  int numP[CLASS] = {3, 1, 1};
+ 
+  int id;
+  while (scanf("%d", &id) != EOF) {
+    int class = id % CLASS;
+    queue[class][(start[class] + length[class]) % QUEUE] = id;
+    length[class]++;
+ 
+    if (enoughPlayer(length, numP)) {
+      for (int c = 0; c < CLASS; c++)
+	for (int p = 0; p < numP[c]; p++) {
+	  printf("%d ", queue[c][start[c] % QUEUE]);
+	  start[c]++;
+	  length[c]--;
+	}
+      printf("\n");
+    }
+  }
+  return 0;
+}
