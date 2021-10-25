0d4c2dada54f6b664b8fc01735669c07b8517710
Pangfeng Liu <pangfeng.liu@gmail.com>
2021-10-25 16:13:54 +0800

    secret expression

---
+++ 10283.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+  int digit;
+  int number = 0;
+  int product = 1;
+  int sum = 0;
+  scanf("%d", &digit);
+  while (digit != 0) {
+    if (digit >= 1 && digit <= 7)
+      number = number * 10 + digit;
+    else {
+      product *= number;
+      if (digit == 8) {
+	sum += product;
+	product = 1;
+      }
+      number = 0;
+    }
+    scanf("%d", &digit);
+  }
+  product *= number;
+  sum += product;
+  printf("%d\n", sum);
+  return 0;
+}
